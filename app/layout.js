f7028cfcf62d10eaf95629c9ea9ef8015aaac147
Charu-Gupta9 <charugupta@Charus-MacBook-Pro.local>
2024-02-17 10:57:01 -0800

    UI Code

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Terra API Demo",
+  title: "TreeHacks Sustainabililty App",
   description: "Use this as a skeleton for your project!",
 };
 
--- app/page.js
+++ app/page.js
@@ -1,16 +1,63 @@
 import Image from "next/image";
-import { useSearchParams } from "next/navigation";
-import ConnectUser from "./components/ConnectUser";
-import RestAPI from "./components/RestAPI";
+import Link from "next/link"
+import MainPage from "./pages/MainPage";
 
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 px-48">
-      <div className="grid grid-cols-[1fr_1fr] gap-12 justify-center items-center w-full">
-        <ConnectUser />
-        <RestAPI />
-      </div>
+    <main>
+      <header className="px-4 lg:px-6 h-14 flex items-center">
+        <Link className="flex items-center justify-center" href="#">
+          <MountainIcon className="h-6 w-6" />
+          <span className="sr-only">Acme Inc</span>
+        </Link>
+        <div>
+         
+
+        {/* <navbar className="text-sm font-medium hover:underline underline-offset-4" /> */}
+        </div>
+    
+        <nav className="ml-auto flex gap-4 sm:gap-6">
+          <Link
+            className="text-sm font-medium hover:underline underline-offset-4"
+            href="/">
+            Home
+          </Link>
+          <Link
+            className="text-sm font-medium hover:underline underline-offset-4"
+            href="/itemChecker">
+            Item Checker
+          </Link>
+          <Link
+            className="text-sm font-medium hover:underline underline-offset-4"
+            href="/findRestaurant">
+            Sustainable Restaurants
+          </Link>
+          <Link
+            className="text-sm font-medium hover:underline underline-offset-4"
+            href="/trackTransport">
+            Daily Transportation Tracker
+          </Link>
+        </nav>
+      </header>
+      <MainPage />
     </main>
+  );
+}
+function MountainIcon(props) {
+  return (
+    (<svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round">
+      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
+    </svg>)
   );
 }
