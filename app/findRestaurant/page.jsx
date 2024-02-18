d5cb8f91da7126ae24dac8e51fb42c3603e902dc
Charu-Gupta9 <charugupta@Charus-MacBook-Pro.local>
2024-02-18 05:55:03 -0800

    changes for restaurant

--- app/findRestaurant/page.jsx
+++ app/findRestaurant/page.jsx
@@ -1,19 +1,28 @@
 "use client"
 import React, { useEffect, useState } from "react";
+import { Input } from "../components/ui/input.jsx"
+import { Button } from "../components/ui/button"
+import axios from 'axios';
 import Link from "next/link"
 
 
 function findRestaurant(){
     const [data, setData] = useState(null);
-    useEffect(() => {
-        fetch('https://treehacks-api.onrender.com/restaurants?address=Stanford%20University')
-          .then(response => response.json())
-          .then(json => setData(json))
-          .catch(error => console.error(error));
-      }, []);
-    if (!data) {
-        return <div>Loading...</div>;
-      }
+    const [clicked, isClicked] = useState(false);
+    const [value, setValue] = useState("");
+
+      function getData (value) {
+        axios.get(`https://treehacks-api.onrender.com/restaurants?address=${value}`).then(res =>{
+          console.log(res);
+          setData(res.data);
+          isClicked(true);
+        });
+    console.log(data);
+        }
+    // if (!data) {
+    //     return <div>Loading...</div>;
+    //   }
+      console.log(clicked);
     return (  
         <><header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
@@ -42,37 +51,42 @@ function findRestaurant(){
             Daily Transportation Tracker
           </Link>
         </nav>
-      </header>  
-      <div className="grid gap-6 items-start w-full lg:px-6">
-      <div className="flex flex-col gap-2">
-        <h1 className="text-3xl font-bold">Sustainable Restaurants</h1>
-        <p className="text-gray-500 dark:text-gray-400">
-          A list of sustainable restaurants along with their names and addresses.
-        </p>
-      </div>
-      <div className="flex flex-col md:gap-4">
-        <div className="grid md:grid-cols-3 gap-4">
-          {
-            data.names.map(items => (
-              <div className="flex flex-col gap-1.5">
-            <h3 className="font-bold">{items.name}</h3>
-            <p className="text-sm">
-              {items.direction}
+      </header><div className="grid gap-6 items-start w-full lg:px-6">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-3xl font-bold">Sustainable Restaurants</h1>
+            <p className="text-gray-500 dark:text-gray-400">
+              A list of sustainable restaurants along with their names and addresses.
             </p>
           </div>
-            ))
-          }
+          <div className="flex w-full max-w-sm items-center space-x-2">
+            <Input className="w-300" placeholder="Search" type="search" value={value} onChange={e => setValue(e.target.value)}/>
+            <Button type="submit" onClick={() => getData(value)}>Search</Button>
+          </div>
         </div>
-        <div>
-        </div>
-        <div className="flex flex-col items-center gap-10">
-          <img src={data.static_map} />
-        </div>
-        </div>
-      
-    </div>    
-       
-        </>
+      {
+        clicked ? 
+        <div className="flex flex-col md:gap-4 pt-10 pl-6">
+          <div className="grid md:grid-cols-3 gap-4">
+            {
+              data.names.map(items => (
+                <div className="flex flex-col gap-1.5">
+              <h3 className="font-bold">{items.name}</h3>
+              <p className="text-sm">
+                {items.direction}
+              </p>
+            </div>
+              ))
+            }
+          </div>
+          <div>
+          </div>
+          <div className="flex flex-col items-center gap-10">
+            <img src={data.static_map} />
+          </div>
+          </div>  
+          : null
+      } 
+      </>
       );
 }
 function MountainIcon(props) {
